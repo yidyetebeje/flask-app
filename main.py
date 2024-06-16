6ea44c3135827e9fc9f496d349643d70935b3e40
yidnekachewtebeje <yidnekachewtebeje@gmail.com>
2024-06-16 13:40:48 +0000

    update

--- main.py
+++ main.py
@@ -1,15 +1,175 @@
 import os
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import tensorflow as tf
+from PIL import Image
+import numpy as np
 
-from flask import Flask, send_file
 
 app = Flask(__name__)
 
-@app.route("/")
+# Model loading and configuration
+model = tf.keras.models.load_model('./trained_classifier.h5') 
+
+# Food class labels 
+class_labels = ['Pizza', 'Burger', 'Sushi', 'Salad', ...]  
+
+# Function to preprocess the uploaded image
+def preprocess_image(image_path):
+    img = Image.open(image_path).resize((224, 224))  # Open and resize the image
+    img_array = np.array(img) 
+     # Preprocess according to your model
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    return img_array
+food_categories = [
+    "apple_pie",
+    "baby_back_ribs",
+    "baklava",
+    "beef_carpaccio",
+    "beef_tartare",
+    "beet_salad",
+    "beignets",
+    "bibimbap",
+    "bread_pudding",
+    "breakfast_burrito",
+    "bruschetta",
+    "caesar_salad",
+    "cannoli",
+    "caprese_salad",
+    "carrot_cake",
+    "ceviche",
+    "cheesecake",
+    "cheese_plate",
+    "chicken_curry",
+    "chicken_quesadilla",
+    "chicken_wings",
+    "chocolate_cake",
+    "chocolate_mousse",
+    "churros",
+    "clam_chowder",
+    "club_sandwich",
+    "crab_cakes",
+    "creme_brulee",
+    "croque_madame",
+    "cup_cakes",
+    "deviled_eggs",
+    "donuts",
+    "dumplings",
+    "edamame",
+    "eggs_benedict",
+    "escargots",
+    "falafel",
+    "filet_mignon",
+    "fish_and_chips",
+    "foie_gras",
+    "french_fries",
+    "french_onion_soup",
+    "french_toast",
+    "fried_calamari",
+    "fried_rice",
+    "frozen_yogurt",
+    "garlic_bread",
+    "gnocchi",
+    "greek_salad",
+    "grilled_cheese_sandwich",
+    "grilled_salmon",
+    "guacamole",
+    "gyoza",
+    "hamburger",
+    "hot_and_sour_soup",
+    "hot_dog",
+    "huevos_rancheros",
+    "hummus",
+    "ice_cream",
+    "lasagna",
+    "lobster_bisque",
+    "lobster_roll_sandwich",
+    "macaroni_and_cheese",
+    "macarons",
+    "miso_soup",
+    "mussels",
+    "nachos",
+    "omelette",
+    "onion_rings",
+    "oysters",
+    "pad_thai",
+    "paella",
+    "pancakes",
+    "panna_cotta",
+    "peking_duck",
+    "pho",
+    "pizza",
+    "pork_chop",
+    "poutine",
+    "prime_rib",
+    "pulled_pork_sandwich",
+    "ramen",
+    "ravioli",
+    "red_velvet_cake",
+    "risotto",
+    "samosa",
+    "sashimi",
+    "scallops",
+    "seaweed_salad",
+    "shrimp_and_grits",
+    "spaghetti_bolognese",
+    "spaghetti_carbonara",
+    "spring_rolls",
+    "steak",
+    "strawberry_shortcake",
+    "sushi",
+    "tacos",
+    "takoyaki",
+    "tiramisu",
+    "tuna_tartare",
+    "waffles"
+]
+
+# Upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Route for home page
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return send_file('src/index.html')
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "No file part"
 
-def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+        file = request.files['file']
+        if file.filename == '':
+            return "No selected file"
 
-if __name__ == "__main__":
-    main()
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+
+            img_array = preprocess_image(filepath)
+            predictions = model.predict(img_array)[0]
+            top_indices = predictions.argsort()[-5:][::-1]  # Indices of top 5
+            top_predictions = [(food_categories[i], predictions[i]) for i in top_indices]  
+    
+            return render_template('result.html', predictions=top_predictions)
+
+    return render_template('index.html')
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    if file:
+        filename = secure_filename(file.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(filepath)
+
+        img_array = preprocess_image(filepath)
+        predictions = model.predict(img_array)[0]
+        top_indices = predictions.argsort()[-5:][::-1]  # Indices of top 5
+        top_predictions = [{"food": food_categories[i], "probability": float(predictions[i])} for i in top_indices]
+        return jsonify({"predictions": top_predictions})
+if __name__ == '__main__':
+    app.run(port=int(os.environ.get('PORT', 0)))
